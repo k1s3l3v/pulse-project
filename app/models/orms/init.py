dabb50d7e5639e890a96ccdc69e3f6e42dd98f2d
Azat Davletshin <46604662+dav1etshin@users.noreply.github.com>
2021-10-28 22:33:51 +0300

    Add files via upload

---
+++ app/models/orms/init.py
@@ -0,0 +1,6 @@
+from .base import BaseORM
+from .project_criterion import ProjectCriterionORM
+from .project_specific_criterion import ProjectSpecificCriterionORM
+from .project_status import ProjectStatusORM
+from .project_criterion_value import ProjectCriterionValueORM
+
---
+++ app/models/orms/project_status.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Date, Integer, JSON
+
+from .base import BaseORM
+from ..mixins import DateORMMixin
+
+
+class ProjectStatusORM(BaseORM, DateORMMixin):
+    project_status_id = Column(Integer, BaseORM.seq, primary_key=True, server_default=BaseORM.seq.next_value())
+    project_id = Column(Integer, nullable=False, unique=True)
+    aggregated_value = Column(Integer, nullable=False)
+    latest_updated_at = Column(Date, nullable=False)
+    latest_updater_id = Column(Integer, nullable=False)
+    latest_grades = Column(JSON, nullable=False)
+
+    def __repr__(self):
+        return f'<project_status {self.name}>'
