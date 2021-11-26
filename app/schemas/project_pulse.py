5be0c2704adeb6329956e310e4f2434d54af1531
k1s3l3v <70627048+k1s3l3v@users.noreply.github.com>
2021-11-26 18:03:01 +0300

    Update project_pulse.py

--- app/schemas/project_pulse.py
+++ app/schemas/project_pulse.py
@@ -9,3 +9,11 @@ from .project_status import ProjectStatus
 class ProjectPulse(TrimModel):
     status: ProjectStatus = Field(None, description='Common project status')
     criteria: List[ProjectSpecificCriterion] = Field(..., description='List of existing criteria for current project')
+
+
+class ProjectPulseUpdate(TrimModel):
+    project_criterion_id: PositiveInt = Field(..., description='The project criterion identifier')
+    date: date_ = Field(..., description='Date of pulse update')
+    value: PositiveInt = Field(1, description='New pulse value')
+    comment: str = Field('', description='Few words about update')
+ 
